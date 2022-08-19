056535716f868ea4eaf2d4afadd2ebe444252253
Dave Charm <davecharm16@gmail.com>
2022-08-19 11:37:36 +0800

    Second Commit to Views

--- application/controllers/Mentors.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	class Mentors extends CI_Controller {
-		public function hello($name, $num)
-		{
-			echo "Hello mentee! Keep up the good work! If you'll finish by $num months, you'll be more hirable! - $name ";
-			$this->load->view('hello');
-		}
-	}
-?>
